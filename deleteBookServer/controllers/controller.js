6a8944650227903af46935ae0751946a0a51fe1d
EverChicas <2373708320101@ingenieria.usac.edu.gt>
2021-06-12 23:27:03 -0600

    add controller

--- deleteBookServer/controllers/controller.js
+++ deleteBookServer/controllers/controller.js
@@ -4,10 +4,11 @@ var mongoose = require('mongoose'),
     Book = mongoose.model('Book');
 
 exports.deleteBookController = function(req, res){
+    console.log(req.body);
     Book.findById(req.body._id, function(err, book){
         book.remove(function(err){
             if(err) return res.send(500, err.message);
-            res.status(200);
+            res.json({status:200});
         });
     });
 };
