b5f9fd62a874bea5c8954677021089a646103445
EverChicas <2373708320101@ingenieria.usac.edu.gt>
2021-06-12 23:17:55 -0600

    make server

---
+++ deleteBookServer/controllers/controller.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var mongoose = require('mongoose'),
+    Book = mongoose.model('Book');
+
+exports.deleteBookController = function(req, res){
+    Book.findById(req.body._id, function(err, book){
+        book.remove(function(err){
+            if(err) return res.send(500, err.message);
+            res.status(200);
+        });
+    });
+};
