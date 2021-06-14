1d8972fbc6cfd92dee3097cd8a0f416aa1c49c5a
samurosales <samurosales@gmail.com>
2021-06-14 16:59:18 -0600

    docker-compose servises added

--- DeleteBookServer/routes/route.js
+++ DeleteBookServer/routes/route.js
@@ -5,4 +5,8 @@ module.exports = function(app) {
   app.route('/book/deleteBook')
     .post(deleteBook.deleteBookController);
 
+  app.route('/')
+    .get(function(req, res) {
+      res.send("Hello World!");
+    })
 };
--- UpdateBookServer/routes/route.js
+++ UpdateBookServer/routes/route.js
@@ -1,8 +1,19 @@
 'use strict';
+
+const { get } = require('../../Compras/src/routes');
+
 module.exports = function(app) {
   var updateBook = require('../controllers/controller');
 
   app.route('/book/updateBook')
     .post(updateBook.updateBookController);
+
+  app.route('/')
+    .get(function(req, res) {
+      res.send("Hello World!");
+    })
+
+
+
 
 };
