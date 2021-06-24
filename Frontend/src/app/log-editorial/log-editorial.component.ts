9fd2d8105a5aea2863abeb6ece8552a6b37a191e
Alejandro <alerod620@gmail.com>
2021-06-24 12:43:54 -0600

    [Bugfix]Cambio de acceso a URLs

--- Frontend/src/app/log-editorial/log-editorial.component.ts
+++ Frontend/src/app/log-editorial/log-editorial.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { log } from '../models/log';
+import { URLs } from '../urls/urls';
 
 @Component({
   selector: 'app-log-editorial',
@@ -10,7 +11,8 @@ import { log } from '../models/log';
 })
 export class LogEditorialComponent implements OnInit {
 
-  url_get: string = 'http://34.69.133.221:47011/bitacora/getBitacora'
+  myURL = new URLs();
+  url_get: string = this.myURL.url_getBitacora;
   public lista_log: log[]  = [];
   error;
 
