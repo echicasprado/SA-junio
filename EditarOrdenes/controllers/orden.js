88a97100450d848e3c61986af3c93d8d35642b7f
Alejandro <alerod620@gmail.com>
2021-06-28 00:41:53 -0600

    Creación de métodos para ver y editar ordenes

---
+++ EditarOrdenes/controllers/orden.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Orden  = mongoose.model('compra');
+
+//Devuelve todos las solicitudes
+exports.editarOrden = function(req, res) {
+	Orden.findOne({_id: req.body._id}, function(err, user) {
+
+		user._id		=   req.body._id
+		user.productos 	= 	req.body.productos,
+		user.usuario	=   req.body.usuario,
+		user.total		= 	req.body.total,
+		user.estado		=   req.body.estado
+
+		user.save(function(err) {
+			if(err) return res.send(500, err.message);
+      res.status(200).json(user);
+		});
+	});
+};
