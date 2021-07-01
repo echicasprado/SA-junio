f22c0388c44512f7dd0f4c258df1c79ce5def505
Alejandro <alerod620@gmail.com>
2021-06-30 23:30:00 -0600

    Frontend de ordenes cliente y ordenes en general

---
+++ Frontend/src/app/ordenes/ordenes.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { URLs } from '../urls/urls';
+import { Orden1 } from '../models/orden';
+
+
+@Component({
+  selector: 'app-ordenes',
+  templateUrl: './ordenes.component.html',
+  styleUrls: ['./ordenes.component.css']
+})
+
+export class OrdenesComponent implements OnInit {
+
+  myURL = new URLs();
+  url: string = this.myURL.url;
+  public listaordenes: Orden1[] = [];
+  error;
+
+  selectedEstado: number;
+
+    estados = [
+        { id: 1, name: 'Aceptada' },
+        { id: 2, name: 'Importada' },
+        { id: 3, name: 'Empaquetada' },
+        { id: 4, name: 'En Camino' },
+        { id: 5, name: 'Entregada' }
+    ];
+
+  constructor(private http: HttpClient, private router : Router) { 
+
+    this.http.get('http://localhost:3000/Ordenes').subscribe((data:Orden1[])=> {//data es la respuesta
+      this.listaordenes = data;
+
+      },error => this.error = error);
+  }
+
+  editarEstado(id, estado){
+    var editado = '{ "_id":"' + id + '", "estado":' + estado + '}';
+    console.log(editado);
+    var res;
+    /*
+    this.http.post(this.url_editar, editado).subscribe((data)=> {//data es la respuesta
+      res = data;
+      },error => this.error = error); */
+  }
+
+  ngOnInit(): void {
+  }
+
+}
